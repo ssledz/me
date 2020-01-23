9fcc1db693bb9d931280fdd0d6d14354abdb2361
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-01-23 01:35:04 +0100

    publish script

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+jekyll clean
+jekyll build
+cd _site
+git init
+gco -b gh-pages
+git add -A
+git commit -m 'pages'
+git remote add origin git@github.com:ssledz/me.git
+git push --force origin gh-pages
